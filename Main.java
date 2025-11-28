a307111d51698a1550f7d77735998418dad0c744
sam <gaburrisamuele@gmail.com>
2025-11-28 09:12:39 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,6 +8,133 @@ import java.io.ObjectOutputStream;
 import java.util.Scanner;
 
 public class Main{
+
+    public static void disegna(int l){
+//        Ingresso: 447, 771
+//        Camera1: 279, 555
+//        Corridoio centro: 467, 348
+//        Tavolo cucina: 659, 391
+//        Sala: 801, 481
+//        Striscia led: (532, 493), (532, 362)
+//        Bagno grande centro: 295, 405
+//        Bagno grande led: (338, 338), (375, 338)
+//        Matrimoniale centro: 225, 147
+//        Matrimoniale abatjour destra: 310, 315
+//        Matrimoniale abatjour sinistra: 145, 315
+//        Bagno piccolo: 486, 147
+//        Camera doppia centro: 769, 244
+//        Camera doppia abatjour sinistra: 704, 106
+//        Camera doppia abatjour destra: 839, 106
+//        Balcone led: (1001, 163), (1001, 544)
+
+        if(l == 1){    // Ingresso
+            Picture dis = new Picture("lampada1.jpeg");
+            dis.grow(-470, -470);
+            dis.translate(-60, 290);
+            dis.draw();
+        }
+
+        if(l == 2){    // Camera1
+            Picture dis = new Picture("lampada1.jpeg");
+            dis.grow(-480, -480);
+            dis.translate(-230, 40);
+            dis.draw();
+        }
+
+        if(l == 3){    // Corridoio centro
+            Picture dis = new Picture("lampada1.jpeg");
+            dis.grow(-480, -480);
+            dis.translate(-40, -130);
+            dis.draw();
+        }
+
+        if(l == 4){    // Tavolo cucina (659, 391)
+            Picture dis = new Picture("lampada1.jpeg");
+            dis.grow(-480, -480);
+            dis.translate(160, -80);
+            dis.draw();
+        }
+
+        if(l == 5){    // Sala (801, 481)
+            Picture dis = new Picture("lampada1.jpeg");
+            dis.grow(-480, -480);
+            dis.translate(300, 10);
+            dis.draw();
+        }
+
+        if(l == 6){    // Striscia led (532, 493)
+            Rectangle r = new Rectangle(532, 362, 7, 131);
+            r.draw();
+        }
+
+        if(l == 7){    // Bagno grande centro (295,405)
+            Picture dis = new Picture("lampada1.jpeg");
+            dis.grow(-480, -480);
+            dis.translate(-210, -70);
+            dis.draw();
+        }
+
+        if(l == 8){    // Bagno grande led sinistra (338,338)
+            Rectangle r = new Rectangle(338, 338, 37, 7);
+            r.draw();
+        }
+
+        if(l == 9){   // Matrimoniale centro (225,147)
+            Picture dis = new Picture("lampada1.jpeg");
+            dis.grow(-480, -480);
+            dis.translate(-280, -330);
+            dis.draw();
+        }
+
+        if(l == 10){   // Matrimoniale abatjour destra (310,315)
+            Picture dis = new Picture("lampada1.jpeg");
+            dis.grow(-480, -480);
+            dis.translate(-195, -165);
+            dis.draw();
+        }
+
+        if(l == 11){   // Matrimoniale abatjour sinistra (145,315)
+            Picture dis = new Picture("lampada1.jpeg");
+            dis.grow(-480, -480);
+            dis.translate(-360, -165);
+            dis.draw();
+        }
+
+        if(l == 12){   // Bagno piccolo (486,147)
+            Picture dis = new Picture("lampada1.jpeg");
+            dis.grow(-480, -480);
+            dis.translate(-30, -330);
+            dis.draw();
+        }
+
+        if(l == 13){   // Camera doppia centro (769,244)
+            Picture dis = new Picture("lampada1.jpeg");
+            dis.grow(-480, -480);
+            dis.translate(270, -230);
+            dis.draw();
+        }
+
+        if(l == 14){   // Camera doppia abatjour sinistra (704,106)
+            Picture dis = new Picture("lampada1.jpeg");
+            dis.grow(-480, -480);
+            dis.translate(210, -370);
+            dis.draw();
+        }
+
+        if(l == 15){   // Camera doppia abatjour destra (839,106)
+            Picture dis = new Picture("lampada1.jpeg");
+            dis.grow(-480, -480);
+            dis.translate(345, -370);
+            dis.draw();
+        }
+
+        if(l == 16){   // Balcone led (1001,163)
+            Rectangle r = new Rectangle(994, 165, 7, 381);
+            r.draw();
+        }
+
+    }
+
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
@@ -15,11 +142,6 @@ public class Main{
 
         Picture p = new Picture("casa.jpg");
         p.draw();
-
-        Picture dis = new Picture("lampada1.jpeg");
-        dis.grow(-470, -470);
-        dis.translate(-20, 360);
-        dis.draw();
 
         boolean flag = true;
 
@@ -38,7 +160,6 @@ public class Main{
             System.out.println("10. Modifica colore lampadina");
             System.out.println("11. Mostra potenza attuale");
             System.out.println("12. Elenco lampadine");
-            System.out.println("13, salva la casa");
             System.out.println("0. Esci");
             System.out.println("=== MENU CASA DOMOTICA ===");
             System.out.println("Scelta: ");
@@ -47,54 +168,116 @@ public class Main{
 
             if (scelta == 1) {
                 //Scelta della lampadina da parte dell'utente
-                Text t = new Text(447, 771, "1");
-                t.draw();
-                Text t1 = new Text(279, 555, "2");
-                t1.draw();
-                Text t2 = new Text(467, 348, "3");
-                t2.draw();
-                Text t3 = new Text(659, 391, "4");
-                t3.draw();
-                Text t4 = new Text(801, 481, "5");
-                t4.draw();
-                Text t5 = new Text(525, 400, "6");
-                t5.draw();
-                Text t6 = new Text(295, 405, "7");
-                t6.draw();
-                Text t7 = new Text(345, 338, "8");
-                t7.draw();
-                Text t8 = new Text(225, 147, "9");
-                t8.draw();
-                Text t9 = new Text(310, 315, "10");
-                t9.draw();
-                Text t10 = new Text(145, 315, "11");
-                t10.draw();
-                Text t11 = new Text(486, 147, "11");
-                t11.draw();
-                Text t12 = new Text(769, 244, "12");
-                t12.draw();
-                Text t13 = new Text(704, 106, "14");
-                t13.draw();
-                Text t14 = new Text(839, 106, "15");
-                t14.draw();
-                Text t15 = new Text(985, 300, "16");
-                t15.draw();
+                {
+                    Text t = new Text(447, 771, "1");
+                    t.draw();
+                    Text t1 = new Text(279, 555, "2");
+                    t1.draw();
+                    Text t2 = new Text(467, 348, "3");
+                    t2.draw();
+                    Text t3 = new Text(659, 391, "4");
+                    t3.draw();
+                    Text t4 = new Text(801, 481, "5");
+                    t4.draw();
+                    Text t5 = new Text(532, 493, "6");
+                    t5.draw();
+                    Text t6 = new Text(295, 405, "7");
+                    t6.draw();
+                    Text t7 = new Text(345, 338, "8");
+                    t7.draw();
+                    Text t8 = new Text(225, 147, "9");
+                    t8.draw();
+                    Text t9 = new Text(310, 315, "10");
+                    t9.draw();
+                    Text t10 = new Text(145, 315, "11");
+                    t10.draw();
+                    Text t11 = new Text(486, 147, "11");
+                    t11.draw();
+                    Text t12 = new Text(769, 244, "12");
+                    t12.draw();
+                    Text t13 = new Text(704, 106, "14");
+                    t13.draw();
+                    Text t14 = new Text(839, 106, "15");
+                    t14.draw();
+                    Text t15 = new Text(985, 300, "16");
+                    t15.draw();
+
+                    t.setText("");
+                    t.draw();
+                    t1.setText("");
+                    t1.draw();
+                    t2.setText("");
+                    t2.draw();
+                    t3.setText("");
+                    t3.draw();
+                    t4.setText("");
+                    t4.draw();
+                    t5.setText("");
+                    t5.draw();
+                    t6.setText("");
+                    t6.draw();
+                    t7.setText("");
+                    t7.draw();
+                    t8.setText("");
+                    t8.draw();
+                    t9.setText("");
+                    t9.draw();
+                    t10.setText("");
+                    t10.draw();
+                    t11.setText("");
+                    t11.draw();
+                    t12.setText("");
+                    t12.draw();
+                    t13.setText("");
+                    t13.draw();
+                    t14.setText("");
+                    t14.draw();
+                    t15.setText("");
+                    t15.draw();
+
+                }
+                
 
                 System.out.print("Nome lampadina: ");
                 String nome = in.next();
-
                 System.out.print("Potenza in watt: ");
                 int pot = in.nextInt();
-
                 System.out.print("Luminosità iniziale (0-100): ");
                 int qta = in.nextInt();
-
                 System.out.print("Colore: ");
                 String colore = in.next();
-
                 System.out.println("Scegli la tua lampadina: ");
-                System.out.println("");
+                System.out.println("Ingresso: 447, 771 (1)\n" +
+                        "camera1 (2)\n" +
+                        "corridoio centro (3)\n" +
+                        "tavolo cucina (4)\n" +
+                        "sala (5)" +
+                        "striscia led cucina (6)" +
+                        "bagno grande centro (7)" +
+                        "bagno grande led (8)\n" +
+                        "matrimoniale centro (9)\n" +
+                        "matrimoniale abatjour destra (10)\n" +
+                        "matrimoniale abatjour sinistra (11)\n" +
+                        "bagno piccolo (12)\n" +
+                        "camera doppia centro (13)\n" +
+                        "camera doppia abatjour sinistra (14)\n" +
+                        "camera doppia abatjour destra (15)\n" +
+                        "balcone led (16)");
 
+                int lampaScelta = in.nextInt();
+                int conta = 0;
+                int[] v = new int[16];
+                for (int i = 0; i < v.length; i++) {
+                    while(lampaScelta == v[i] || lampaScelta > 16 || lampaScelta <= 0){
+                        //reinserimento della lampadina in caso l'utente abbia messo un valore gia presente o diverso da quelli presenti
+                        System.out.println("La posizione è gia occupata, inserisci una nuova lampadina: ");
+                        lampaScelta = in.nextInt();
+                    }
+                    v[conta] = lampaScelta;
+                    conta++;
+                }
+
+                disegna(lampaScelta);
                 LampadinaIntelligente lamp = new LampadinaIntelligente(nome, pot, qta, colore);
 
                 if (casa.add(lamp))
@@ -195,14 +378,6 @@ public class Main{
                 System.out.println("Lampadine presenti: ");
                 for (int i = 0; i < casa.getElencoLampadine().size(); i++) {
                     System.out.println("- " + casa.getElencoLampadine().get(i));
-                }
-
-            } else if (scelta == 13) {
-                try {
-                    ObjectOutputStream bs = new ObjectOutputStream(new FileOutputStream("casa.jpg"));
-                    bs.close();
-                } catch (IOException e) {
-                    System.out.println("Il file non è stato trovato, oppure non esiste");
                 }
 
             } else if (scelta == 0) {
